3e1f3e59a5d42fc6eb5b4a6c980e6fd0a0347ede
AdisornBoonard <bigzazaiiiii@gmail.com>
2026-06-26 17:23:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -325,8 +325,8 @@ with col_left:
         total_capital, available_capital = get_balance()
         st.metric("เงินทุนสุทธิในกระดาน", f"${total_capital}")
         
-        base_mgn = st.number_input("Margin ไม้แรก ($)", value=1.00, format="%.4f")  
-        daily_add = st.number_input("เพิ่ม Margin วันละ ($)", value=3.00, format="%.4f")
+        base_mgn = st.number_input("Margin ไม้แรก ($)", value=0.02, format="%.4f")  
+        daily_add = st.number_input("เพิ่ม Margin วันละ ($)", value=0.06, format="%.4f")
         lev = st.number_input("Leverage (x)", value=250, min_value=1, max_value=250)
         max_t = st.number_input("เปิดสูงสุด (ต่อฝั่ง)", value=10)
         
